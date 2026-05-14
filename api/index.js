7a4518424233db3c535592854b976b0c5004d2d7
Nguyen Thanh Trung <trungvip12356790@gmail.com>
2026-05-14 18:02:28 +0700

    update feature

--- api/index.js
+++ api/index.js
@@ -109,6 +109,11 @@ app.post("/verify-otp", (req, res) => {
 });
 
 // ================= START SERVER =================
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
 });
+
+module.exports = app;
